1a0e1a0a8c1ac01634a29dfad35e14b5f689d0a7
ZacheryCalahan <zacherycalahan33@gmail.com>
2019-01-19 17:41:48 -0500

    frame

--- src/app/App.java
+++ src/app/App.java
@@ -2,9 +2,15 @@ package app;
 
 import javax.swing.*;
 
-    public class App {
-        public static void main(String[] args) {
-        System.out.println("Thing");    
+public class App {
+    //Init Vars
+    
+    //Create Objects
+    JFrame home = new JFrame();
 
+
+
+    public static void main(String[] args) {
+        System.out.println("Init");    
     }
 }
